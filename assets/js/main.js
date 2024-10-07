dbd90067c321a5a7594f8cfb8f4cc542b105561f
Raditya Ananda Rohman <anandarohman2@gmail.com>
2024-10-07 10:26:06 +0700

    delete main.js

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-const hamburgerMenu = document.querySelector(".hamburger-menu")
-const dropdownNav = document.querySelector(".dropdown-menu")
-
-hamburgerMenu.addEventListener("click", mobileMenu);
-
-const mobileMenu = () => {
-    hamburgerMenu.classList.toggle("active")
-    dropdownNav.classList.toggle("active")
-}
