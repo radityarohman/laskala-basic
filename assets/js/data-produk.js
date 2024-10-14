925bc0f79ba4bbbcb85c431d98126392777f2560
Raditya Ananda Rohman <anandarohman2@gmail.com>
2024-10-14 12:13:11 +0700

    update data produk

--- assets/js/data-produk.js
+++ assets/js/data-produk.js
@@ -30,6 +30,19 @@ let products = {
             imageLain: [
                 "/assets/images/products/crewneck/rio-black-2.webp",
                 "/assets/images/products/crewneck/rio-black-3.webp"
+            ],
+            linkShopee: "https://shopee.co.id/Laskala-Basic-Crewneck-Casual-Pria-Rio-Black-i.724380667.22684014792?sp_atk=4c235efc-b947-4123-9999-f98ca41b0af3&xptdk=4c235efc-b947-4123-9999-f98ca41b0af3",
+            deskripsiProduk: [
+                "Warna : Hitam",
+                "Bahan Dasar Baju : Bahan Fleece cotton memberikan rasa yang lembut, hangat, tebal, serta halus. Sehingga ketika dikenakan akan memberikan kesan nyaman dan hangat",
+                "Kerah : Kerah Bulat",
+                "Terdapat detail saku tempel dengan ritsleting jenis vislon bahan metal",
+                "Motif Lengan : Burung Pecuk, Parang, Kawung, Burung Murai, Parang Coklat Muda, Ukel Cantel",
+                "Silver Tag yang tidak berkarat membuat lebih elegan dan original",
+                "Ukuran : S, M, L, XL & XXL",
+                "Exclusive packaging (reusable travel bag)",
+                "Semua varian batik di foto menggunakan kamera A7S II",
+                "Real pict 100%",
             ]
         },
         {
@@ -41,6 +54,18 @@ let products = {
             imageLain: [
                 "/assets/images/products/crewneck/roma-2.webp",
                 "/assets/images/products/crewneck/roma-3.webp"
+            ],
+            linkShopee: "https://shopee.co.id/Laskala-Basic-Roma-Crewneck-i.724380667.22058511871?sp_atk=0ef01455-db65-44aa-a73a-93235ea3d604&xptdk=0ef01455-db65-44aa-a73a-93235ea3d604",
+            deskripsiProduk: [
+                "Warna : Abu - Abu Muda",
+                "Bahan Dasar Baju : Bahan Fleece cotton memberikan rasa yang lembut, hangat, tebal, serta halus. Sehingga ketika dikenakan akan memberikan kesan nyaman dan hangat",
+                "Kerah : Kerah Bulat",
+                "Terdapat detail saku tempel dengan ritsleting jenis vislon bahan metal",
+                "Motif Lengan : Burung Merak, Ukel Cantel & Bunga Sakura (Batik Damar)",
+                "Ukuran : S, M, L, XL & XXL",
+                "Exclusive packaging (reusable travel bag)",
+                "Semua varian batik di foto menggunakan kamera A7S II",
+                "Real pict 100%",
             ]
         },
         {
@@ -52,17 +77,38 @@ let products = {
             imageLain: [
                 "/assets/images/products/crewneck/rote-navy-2.webp",
                 "/assets/images/products/crewneck/rote-navy-3.webp"
+            ],
+            linkShopee: "https://shopee.co.id/Laskala-Basic-Crewneck-Casual-Pria-Rote-Navy-i.724380667.24462624292?sp_atk=2fcae44f-d228-4067-9bc1-df89c41f19e9&xptdk=2fcae44f-d228-4067-9bc1-df89c41f19e9",
+            deskripsiProduk: [
+                "Warna : Dongker",
+                "Bahan Dasar Baju : Bahan Fleece cotton memberikan rasa yang lembut, hangat, tebal, serta halus. Sehingga ketika dikenakan akan memberikan kesan nyaman dan hangat",
+                "Kerah : Kerah Bulat",
+                "Silver Tag yang tidak berkarat membuat lebih elegan dan original",
+                "Ukuran : S, M, L, XL & XXL",
+                "Exclusive packaging (reusable travel bag)",
+                "Semua varian batik di foto menggunakan kamera A7S II",
+                "Real pict 100%",
             ]
         },
         {
             id: 5,
             namaProduk: "Bruno Hoodie",
             kategori: "Hoodie",
-            harga: "Rp. 345.000",
+            harga: "Rp. 375.000",
             image: "/assets/images/products/hoodie/bruno-hoodie-1.webp",
             imageLain: [
                 "/assets/images/products/hoodie/bruno-hoodie-2.webp",
                 "/assets/images/products/hoodie/bruno-hoodie-3.webp",
+            ],
+            linkShopee: "https://shopee.co.id/Laskala-Basic-Bruno-Hoodie-i.724380667.22062717855?sp_atk=ceb95636-2e02-419b-8d60-8596bb933d0a&xptdk=ceb95636-2e02-419b-8d60-8596bb933d0a",
+            deskripsiProduk: [
+                "Warna : Hitam",
+                "Bahan Dasar Baju : Premium Cotton Fleece. Cotton fleece pada bagian luar dan dalam permukaan kain terasa lembut, sehingga nyaman ketika digunakan",
+                "Motif Lengan : Kombinasi batik mahama dengan nuansa sewarna pada bagian dada yang membuat kesan badan lebih bidang namun tetap tampil stylish",
+                "Ukuran : S, M, L, XL & XXL",
+                "Exclusive packaging (reusable travel bag)",
+                "Semua varian batik di foto menggunakan kamera A7S II",
+                "Real pict 100%",
             ]
         },
         {
@@ -74,14 +120,38 @@ let products = {
             imageLain: [
                 "/assets/images/products/hoodie/basic-dafa-2.webp",
                 "/assets/images/products/hoodie/basic-dafa-3.webp",
+            ],
+            linkShopee: "https://shopee.co.id/Laskala-Basic-Hoodie-Dafa-i.724380667.18287314792?sp_atk=cac332da-6ac3-4caa-a9ca-ac548cafd704&xptdk=cac332da-6ac3-4caa-a9ca-ac548cafd704",
+            deskripsiProduk: [
+                "Warna : Hitam",
+                "Bahan Dasar Baju : Premium Cotton Fleece. Cotton fleece pada bagian luar dan dalam permukaan kain terasa lembut, sehingga nyaman ketika digunakan",
+                "Motif : Memiliki aksen batik sandro dan damar pada bagian pada bagian tertentu, memberikan tampilan yang unik dan mempesona.",
+                "Ukuran : S, M, L, XL & XXL",
+                "Exclusive packaging (reusable travel bag)",
+                "Semua varian batik di foto menggunakan kamera A7S II",
+                "Real pict 100%",
             ]
         },
         {
             id: 7,
             namaProduk: "Hoodie Casual Pria Deon Navy",
             kategori: "Hoodie",
-            harga: "Rp. 345.000",
-            image: "/assets/images/products/hoodie/deon-navy-1.webp"
+            harga: "Rp. 375.000",
+            image: "/assets/images/products/hoodie/deon-navy-1.webp",
+            imageLain: [
+                "/assets/images/products/hoodie/deon-navy-2.webp",
+                "/assets/images/products/hoodie/deon-navy-3.webp",
+            ],
+            linkShopee: "https://shopee.co.id/Laskala-Basic-Hoodie-Casual-Pria-Deon-Navy-i.724380667.24853230458?sp_atk=b54cfb9f-ee29-406c-b219-85217a9a2984&xptdk=b54cfb9f-ee29-406c-b219-85217a9a2984",
+            deskripsiProduk: [
+                "Warna : Dongker",
+                "Bahan Dasar Baju : Premium Cotton Fleece. Cotton fleece pada bagian luar dan dalam permukaan kain terasa lembut, sehingga nyaman ketika digunakan",
+                "Motif Lengan : Memiliki aksen batik pedro dan windu di bagian lengan dan dada yang membuat desain batik semakin elegan namun tetap dapat digunakan sebagai daily wear.",
+                "Ukuran : S, M, L, XL & XXL",
+                "Exclusive packaging (reusable travel bag)",
+                "Semua varian batik di foto menggunakan kamera A7S II",
+                "Real pict 100%",
+            ]  
         },
         {
             id: 8,
@@ -92,6 +162,16 @@ let products = {
             imageLain: [
                 "/assets/images/products/hoodie/jaden-green-2.webp",
                 "/assets/images/products/hoodie/jaden-green-3.webp",
+            ],
+            linkShopee: "https://shopee.co.id/Laskala-Basic-Hoodie-Casual-Pria-Jaden-Green-i.724380667.25608069100?sp_atk=4c16d5b9-db97-48df-aed0-707ed378446e&xptdk=4c16d5b9-db97-48df-aed0-707ed378446e",
+            deskripsiProduk: [
+                "Warna : Hijau",
+                "Bahan Dasar Baju : Premium Cotton Fleece. Cotton fleece pada bagian luar dan dalam permukaan kain terasa lembut, sehingga nyaman ketika digunakan",
+                "Motif Lengan : Memiliki aksen Kombinasi batik sandro & kamal pada bagian badan dan lengan yang membuat kesan badan lebih bidang namun tetap tampil stylish",
+                "Ukuran : S, M, L, XL & XXL",
+                "Exclusive packaging (reusable travel bag)",
+                "Semua varian batik di foto menggunakan kamera A7S II",
+                "Real pict 100%",
             ]
         },
         {
@@ -103,6 +183,16 @@ let products = {
             imageLain: [
                 "/assets/images/products/hoodie/maroon-hoodie-2.webp",
                 "/assets/images/products/hoodie/maroon-hoodie-3.webp",
+            ],
+            linkShopee: "https://shopee.co.id/Laskala-Basic-Hoodie-Casual-Pria-Aka-Maroon-i.724380667.25908056559?sp_atk=c7a1ddef-f1fe-457c-809d-75c61a475b81&xptdk=c7a1ddef-f1fe-457c-809d-75c61a475b81",
+            deskripsiProduk: [
+                "Warna : Maroon",
+                "Bahan Dasar Baju : Premium Cotton Fleece. Cotton fleece pada bagian luar dan dalam permukaan kain terasa lembut, sehingga nyaman ketika digunakan",
+                "Motif Lengan : Memiliki aksen batik simba di pada bagian badan depan dan lengan menjadikan design lebih harmonis",
+                "Ukuran : S, M, L, XL & XXL",
+                "Exclusive packaging (reusable travel bag)",
+                "Semua varian batik di foto menggunakan kamera A7S II",
+                "Real pict 100%",
             ]
         },
         {
@@ -114,6 +204,16 @@ let products = {
             imageLain: [
                 "/assets/images/products/jaket/aro-jaket-2.webp",
                 "/assets/images/products/jaket/aro-jaket-3.webp",
+            ],
+            linkShopee: "https://shopee.co.id/Laskala-Basic-Jaket-Aro-i.724380667.25558317191?sp_atk=cf5baad7-6ebb-4898-a165-8da13eda12c4&xptdk=cf5baad7-6ebb-4898-a165-8da13eda12c4",
+            deskripsiProduk: [
+                "Warna : Biru",
+                "Bahan Dasar Baju : Taslan bahan berkualitas premium, ringan dan tahan air menjadikannya pilihan yang ideal untuk petualangan outdoor dan aktivitas sehari-hari.",
+                "Motif : Memiliki aksen batik pedro pada bagian pada bagian tertentu, memberikan tampilan yang unik dan mempesona.",
+                "Ukuran : S, M, L, XL & XXL",
+                "Exclusive packaging (reusable travel bag)",
+                "Semua varian batik di foto menggunakan kamera A7S II",
+                "Real pict 100%",
             ]
         },
         {
@@ -125,6 +225,16 @@ let products = {
             imageLain: [
                 "/assets/images/products/jaket/jaket-taksa-2.webp",
                 "/assets/images/products/jaket/jaket-taksa-3.webp",
+            ],
+            linkShopee: "https://shopee.co.id/Laskala-Basic-Jaket-Taksa-i.724380667.24255712645?sp_atk=917bb810-9357-4fea-ac63-df7602f6a00e",
+            deskripsiProduk: [
+                "Warna : Maroon",
+                "Bahan Dasar Baju : Taslan bahan berkualitas premium, ringan dan tahan air menjadikannya pilihan yang ideal untuk petualangan outdoor dan aktivitas sehari-hari.",
+                "Motif : Kombinasi batik pada bagian badan depan dan lengan menjadikan design lebih harmonis",
+                "Ukuran : S, M, L, XL & XXL",
+                "Exclusive packaging (reusable travel bag)",
+                "Semua varian batik di foto menggunakan kamera A7S II",
+                "Real pict 100%",
             ]
         },
         {
@@ -136,6 +246,16 @@ let products = {
             imageLain: [
                 "/assets/images/products/jaket/nanda-maroon-2.webp",
                 "/assets/images/products/jaket/nanda-maroon-3.webp",
+            ],
+            linkShopee: "https://shopee.co.id/Laskala-Basic-Jaket-Coach-Pria-Nanda-Maroon-i.724380667.25861329938?sp_atk=16d70f67-80aa-495e-93ce-20480930d782",
+            deskripsiProduk: [
+                "Warna : Maroon",
+                "Bahan Dasar Baju : Bahan parasut memberikan kelebihan ringan dan perlindungan terhadap angin, menjadikannya jaket yang ideal untuk berbagai musim.",
+                "Motif Jaket : Jaket kombinasi batik mahama dibuat untuk menciptakan kesan elegan yang memadukan nuansa batik klasik dengan desain jaket yang nyaman.",
+                "Ukuran : S, M, L, XL & XXL",
+                "Exclusive packaging (reusable travel bag)",
+                "Semua varian batik di foto menggunakan kamera A7S II",
+                "Real pict 100%",
             ]
         },
         {
