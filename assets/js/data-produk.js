2cf2efde3b5cf9ee2a115296ba9fbb4124d93bf0
Raditya Ananda Rohman <anandarohman2@gmail.com>
2024-10-14 10:26:43 +0700

    add detail-produk page

---
+++ assets/js/data-produk.js
@@ -0,0 +1,264 @@
+let products = {
+    data: [
+        {
+            id: 1,
+            namaProduk: "Crewneck Casual Pria Arthur Grey",
+            kategori: "Crewneck",
+            harga: "Rp. 345.000",
+            image: "/assets/images/products/crewneck/arthur-grey-1.webp",
+            imageLain: [
+                "/assets/images/products/crewneck/arthur-grey-2.webp",
+                "/assets/images/products/crewneck/arthur-grey-3.webp"
+            ]
+        },
+        {
+            id: 2,
+            namaProduk: "Crewneck Casual Pria Rio Black",
+            kategori: "Crewneck",
+            harga: "Rp. 330.000",
+            image: "/assets/images/products/crewneck/rio-black-1.webp",
+            imageLain: [
+                "/assets/images/products/crewneck/rio-black-2.webp",
+                "/assets/images/products/crewneck/rio-black-3.webp"
+            ]
+        },
+        {
+            id: 3,
+            namaProduk: "Crewneck Casual Pria Roma",
+            kategori: "Crewneck",
+            harga: "Rp. 347.000",
+            image: "/assets/images/products/crewneck/roma-1.webp",
+            imageLain: [
+                "/assets/images/products/crewneck/roma-2.webp",
+                "/assets/images/products/crewneck/roma-3.webp"
+            ]
+        },
+        {
+            id: 4,
+            namaProduk: "Crewneck Casual Pria Rote Navy",
+            kategori: "Crewneck",
+            harga: "Rp. 330.000",
+            image: "/assets/images/products/crewneck/rote-navy-1.webp",
+            imageLain: [
+                "/assets/images/products/crewneck/rote-navy-2.webp",
+                "/assets/images/products/crewneck/rote-navy-3.webp"
+            ]
+        },
+        {
+            id: 5,
+            namaProduk: "Bruno Hoodie",
+            kategori: "Hoodie",
+            harga: "Rp. 345.000",
+            image: "/assets/images/products/hoodie/bruno-hoodie-1.webp",
+            imageLain: [
+                "/assets/images/products/hoodie/bruno-hoodie-2.webp",
+                "/assets/images/products/hoodie/bruno-hoodie-3.webp",
+            ]
+        },
+        {
+            id: 6,
+            namaProduk: "Basic Hoodie Dafa",
+            kategori: "Hoodie",
+            harga: "Rp. 375.000",
+            image: "/assets/images/products/hoodie/basic-dafa-1.webp",
+            imageLain: [
+                "/assets/images/products/hoodie/basic-dafa-2.webp",
+                "/assets/images/products/hoodie/basic-dafa-3.webp",
+            ]
+        },
+        {
+            id: 7,
+            namaProduk: "Hoodie Casual Pria Deon Navy",
+            kategori: "Hoodie",
+            harga: "Rp. 345.000",
+            image: "/assets/images/products/hoodie/deon-navy-1.webp"
+        },
+        {
+            id: 8,
+            namaProduk: "Hoodie Casual Pria Jaden Green",
+            kategori: "Hoodie",
+            harga: "Rp. 345.000",
+            image: "/assets/images/products/hoodie/jaden-green-1.webp",
+            imageLain: [
+                "/assets/images/products/hoodie/jaden-green-2.webp",
+                "/assets/images/products/hoodie/jaden-green-3.webp",
+            ]
+        },
+        {
+            id: 9,
+            namaProduk: "Hoodie Casual Pria Aka Maroon",
+            kategori: "Hoodie",
+            harga: "Rp. 345.000",
+            image: "/assets/images/products/hoodie/maroon-hoodie-1.webp",
+            imageLain: [
+                "/assets/images/products/hoodie/maroon-hoodie-2.webp",
+                "/assets/images/products/hoodie/maroon-hoodie-3.webp",
+            ]
+        },
+        {
+            id: 10,
+            namaProduk: "Jaket Aro",
+            kategori: "Jaket",
+            harga: "Rp. 320.000",
+            image: "/assets/images/products/jaket/aro-jaket-1.webp",
+            imageLain: [
+                "/assets/images/products/jaket/aro-jaket-2.webp",
+                "/assets/images/products/jaket/aro-jaket-3.webp",
+            ]
+        },
+        {
+            id: 11,
+            namaProduk: "Jaket Taksa",
+            kategori: "Jaket",
+            harga: "Rp. 295.000",
+            image: "/assets/images/products/jaket/jaket-taksa-1.webp",
+            imageLain: [
+                "/assets/images/products/jaket/jaket-taksa-2.webp",
+                "/assets/images/products/jaket/jaket-taksa-3.webp",
+            ]
+        },
+        {
+            id: 12,
+            namaProduk: "Jaket Coach Pria Nanda Maroon",
+            kategori: "Jaket",
+            harga: "Rp. 320.000",
+            image: "/assets/images/products/jaket/nanda-maroon-1.webp",
+            imageLain: [
+                "/assets/images/products/jaket/nanda-maroon-2.webp",
+                "/assets/images/products/jaket/nanda-maroon-3.webp",
+            ]
+        },
+        {
+            id: 13,
+            namaProduk: "Nero Jaket Waterproof",
+            kategori: "Jaket",
+            harga: "Rp. 325.000",
+            image: "/assets/images/products/jaket/nero-jaket-1.webp",
+            imageLain: [
+                "/assets/images/products/jaket/nero-jaket-2.webp",
+                "/assets/images/products/jaket/nero-jaket-3.webp",
+            ]
+        },
+        {
+            id: 14,
+            namaProduk: "Jaket Coach Pria Valo Black",
+            kategori: "Jaket",
+            harga: "Rp. 320.000",
+            image: "/assets/images/products/jaket/valo-black-1.webp",
+            imageLain: [
+                "/assets/images/products/jaket/valo-black-2.webp",
+                "/assets/images/products/jaket/valo-black-3.webp",
+            ]
+        },
+        {
+            id: 15,
+            namaProduk: "Outer Kimono Pria Shuji Sea Salt",
+            kategori: "Kimono",
+            harga: "Rp. 323.000",
+            image: "/assets/images/products/kimono/shuji-sea-1.webp",
+            imageLain: [
+                "/assets/images/products/kimono/shuji-sea-2.webp",
+                "/assets/images/products/kimono/shuji-sea-3.webp",
+            ]
+        },
+        {
+            id: 16,
+            namaProduk: "Outer Kimono Pria Yuji Navy",
+            kategori: "Kimono",
+            harga: "Rp. 320.000",
+            image: "/assets/images/products/kimono/yuji-navy-1.webp",
+            imageLain: [
+                "/assets/images/products/kimono/yuji-navy-2.webp",
+                "/assets/images/products/kimono/yuji-navy-3.webp",
+            ]
+        },
+        {
+            id: 17,
+            namaProduk: "Baseball Shirt Pria Alan Black",
+            kategori: "Shirt",
+            harga: "Rp. 323.000",
+            image: "/assets/images/products/shirt/baseball-shirt-1.webp",
+            imageLain: [
+                "/assets/images/products/shirt/baseball-shirt-2.webp",
+                "/assets/images/products/shirt/baseball-shirt-3.webp",
+            ]
+        },
+        {
+            id: 18,
+            namaProduk: "Bowling Shirt Pria Alan Black",
+            kategori: "Shirt",
+            harga: "Rp. 326.000",
+            image: "/assets/images/products/shirt/bowling-shirt-1.webp",
+            imageLain: [
+                "/assets/images/products/shirt/bowling-shirt-2.webp",
+                "/assets/images/products/shirt/bowling-shirt-3.webp",
+            ]
+        },
+        {
+            id: 19,
+            namaProduk: "Jumper Shirt Pria Nofel Black",
+            kategori: "Shirt",
+            harga: "Rp. 370.000",
+            image: "/assets/images/products/shirt/jumper-shirt-1.webp",
+            imageLain: [
+                "/assets/images/products/shirt/jumper-shirt-2.webp",
+                "/assets/images/products/shirt/jumper-shirt-3.webp",
+            ]
+        },
+        {
+            id: 20,
+            namaProduk: "Kemeja Lengan Pendek Nix",
+            kategori: "Shirt",
+            harga: "Rp. 320.000",
+            image: "/assets/images/products/shirt/kemeja-lengan-pendek-nix-1.webp",
+            imageLain: [
+                "/assets/images/products/shirt/kemeja-lengan-pendek-nix-2.webp",
+                "/assets/images/products/shirt/kemeja-lengan-pendek-nix-3.webp",
+            ]
+        },
+        {
+            id: 21,
+            namaProduk: "Kemeja Lengan Pendek Zada",
+            kategori: "Shirt",
+            harga: "Rp. 347.000",
+            image: "/assets/images/products/shirt/kemeja-lengan-pendek-zada-1.webp",
+            imageLain: [
+                "/assets/images/products/shirt/kemeja-lengan-pendek-zada-2.webp",
+                "/assets/images/products/shirt/kemeja-lengan-pendek-zada-3.webp",
+            ]
+        },
+        {
+            id: 22,
+            namaProduk: "Kemeja Lengan Pendek Otto",
+            kategori: "Shirt",
+            harga: "Rp. 347.000",
+            image: "/assets/images/products/shirt/kemeja-lengan-pendek-otto-1.webp",
+            imageLain: [
+                "/assets/images/products/shirt/kemeja-lengan-pendek-otto-2.webp",
+                "/assets/images/products/shirt/kemeja-lengan-pendek-otto-3.webp",
+            ]
+        },
+        {
+            id: 23,
+            namaProduk: "Kemeja Lengan Panjang Kandra",
+            kategori: "Shirt",
+            harga: "Rp. 320.000",
+            image: "/assets/images/products/shirt/kemeja-panjang1.webp",
+            imageLain: [
+                "/assets/images/products/shirt/kemeja-panjang2.webp",
+                "/assets/images/products/shirt/kemeja-panjang3.webp",
+            ]
+        },
+        {
+            id: 24,
+            namaProduk: "Basic Jumper Shirt Pria Kora Grey",
+            kategori: "Shirt",
+            harga: "Rp. 320.000",
+            image: "/assets/images/products/shirt/laskala-basic-jumper-grey1.webp",
+            imageLain: [
+                "/assets/images/products/shirt/laskala-basic-jumper-grey2.webp",
+                "/assets/images/products/shirt/laskala-basic-jumper-grey3.webp",
+            ]
+        },
+    ]
+};
--- assets/js/produk.js
+++ assets/js/produk.js
@@ -1,171 +1,21 @@
-let products = {
-    data: [
-        {
-            namaProduk: "Crewneck Casual Pria Arthur Grey",
-            kategori: "Crewneck",
-            harga: "Rp. 345.000",
-            image: "/assets/images/products/crewneck/arthur-grey-1.webp"
-        },
-        {
-            namaProduk: "Crewneck Casual Pria Rio Black",
-            kategori: "Crewneck",
-            harga: "Rp. 330.000",
-            image: "/assets/images/products/crewneck/rio-black-1.webp"
-        },
-        {
-            namaProduk: "Crewneck Casual Pria Roma",
-            kategori: "Crewneck",
-            harga: "Rp. 347.000",
-            image: "/assets/images/products/crewneck/roma-1.webp"
-        },
-        {
-            namaProduk: "Crewneck Casual Pria Rote Navy",
-            kategori: "Crewneck",
-            harga: "Rp. 330.000",
-            image: "/assets/images/products/crewneck/rote-navy-1.webp"
-        },
-        {
-            namaProduk: "Bruno Hoodie",
-            kategori: "Hoodie",
-            harga: "Rp. 345.000",
-            image: "/assets/images/products/hoodie/bruno-hoodie-1.webp"
-        },
-        {
-            namaProduk: "Basic Hoodie Dafa",
-            kategori: "Hoodie",
-            harga: "Rp. 375.000",
-            image: "/assets/images/products/hoodie/basic-dafa-1.webp"
-        },
-        {
-            namaProduk: "Hoodie Casual Pria Deon Navy",
-            kategori: "Hoodie",
-            harga: "Rp. 345.000",
-            image: "/assets/images/products/hoodie/deon-navy-1.webp"
-        },
-        {
-            namaProduk: "Hoodie Casual Pria Jaden Green",
-            kategori: "Hoodie",
-            harga: "Rp. 345.000",
-            image: "/assets/images/products/hoodie/jaden-green-1.webp"
-        },
-        {
-            namaProduk: "Hoodie Casual Pria Aka Maroon",
-            kategori: "Hoodie",
-            harga: "Rp. 345.000",
-            image: "/assets/images/products/hoodie/maroon-hoodie-1.webp"
-        },
-        {
-            namaProduk: "Jaket Aro",
-            kategori: "Jaket",
-            harga: "Rp. 320.000",
-            image: "/assets/images/products/jaket/aro-jaket-1.webp"
-        },
-        {
-            namaProduk: "Jaket Taksa",
-            kategori: "Jaket",
-            harga: "Rp. 295.000",
-            image: "/assets/images/products/jaket/jaket-taksa-1.webp"
-        },
-        {
-            namaProduk: "Jaket Coach Pria Nanda Maroon",
-            kategori: "Jaket",
-            harga: "Rp. 320.000",
-            image: "/assets/images/products/jaket/nanda-maroon-1.webp"
-        },
-        {
-            namaProduk: "Nero Jaket Waterproof",
-            kategori: "Jaket",
-            harga: "Rp. 325.000",
-            image: "/assets/images/products/jaket/nero-jaket-1.webp"
-        },
-        {
-            namaProduk: "Jaket Coach Pria Valo Black",
-            kategori: "Jaket",
-            harga: "Rp. 320.000",
-            image: "/assets/images/products/jaket/valo-black-1.webp"
-        },
-        {
-            namaProduk: "Outer Kimono Pria Shuji Sea Salt",
-            kategori: "Kimono",
-            harga: "Rp. 323.000",
-            image: "/assets/images/products/kimono/shuji-sea-1.webp"
-        },
-        {
-            namaProduk: "Outer Kimono Pria Yuji Navy",
-            kategori: "Kimono",
-            harga: "Rp. 320.000",
-            image: "/assets/images/products/kimono/yuji-navy-1.webp"
-        },
-        {
-            namaProduk: "Baseball Shirt Pria Alan Black",
-            kategori: "Shirt",
-            harga: "Rp. 323.000",
-            image: "/assets/images/products/shirt/baseball-shirt-1.webp"
-        },
-        {
-            namaProduk: "Bowling Shirt Pria Alan Black",
-            kategori: "Shirt",
-            harga: "Rp. 326.000",
-            image: "/assets/images/products/shirt/bowling-shirt-1.webp"
-        },
-        {
-            namaProduk: "Jumper Shirt Pria Nofel Black",
-            kategori: "Shirt",
-            harga: "Rp. 370.000",
-            image: "/assets/images/products/shirt/jumper-shirt-1.webp"
-        },
-        {
-            namaProduk: "Kemeja Lengan Pendek Nix",
-            kategori: "Shirt",
-            harga: "Rp. 320.000",
-            image: "/assets/images/products/shirt/kemeja-lengan-pendek-nix-1.webp"
-        },
-        {
-            namaProduk: "Kemeja Lengan Pendek Zada",
-            kategori: "Shirt",
-            harga: "Rp. 347.000",
-            image: "/assets/images/products/shirt/kemeja-lengan-pendek-zada-1.webp"
-        },
-        {
-            namaProduk: "Kemeja Lengan Pendek Otto",
-            kategori: "Shirt",
-            harga: "Rp. 347.000",
-            image: "/assets/images/products/shirt/kemeja-lengan-pendek-otto-1.webp"
-        },
-        {
-            namaProduk: "Kemeja Lengan Panjang Kandra",
-            kategori: "Shirt",
-            harga: "Rp. 320.000",
-            image: "/assets/images/products/shirt/kemeja-panjang1.webp"
-        },
-        {
-            namaProduk: "Basic Jumper Shirt Pria Kora Grey",
-            kategori: "Shirt",
-            harga: "Rp. 320.000",
-            image: "/assets/images/products/shirt/laskala-basic-jumper-grey1.webp"
-        },
-    ]
-};
-
 function displayProducts(filter) {
     const bigProductList = document.getElementById("big-product-list");
     const smallProductList = document.getElementById("small-product-list");
+    let = bigCardCount = 0;
 
     bigProductList.innerHTML = ""; 
     smallProductList.innerHTML = "";
 
     let filteredProducts = products.data;
 
-    
     if (filter !== "Semua") {
         filteredProducts = products.data.filter(product => product.kategori === filter);
     }
 
-    filteredProducts.forEach((product, index) => {
-        
+    filteredProducts.forEach((product) => {
         let productCardBig = `
             <div class="prod-card-big" data-aos="fade-up" data-aos-duration="400">
-                <a href="#">
+                <a href="detail-produk.html?product=${product.id}">
                     <img
                         src="${product.image}"
                         alt="${product.namaProduk}"
@@ -179,10 +29,10 @@ function displayProducts(filter) {
                 </a>
             </div>
         `;
-
+    
         let productCardSmall = `
             <div class="prod-card-sm" data-aos="fade-up" data-aos-duration="550">
-                <a href="#">
+                <a href="detail-produk.html?product=${product.id}">
                     <img
                         src="${product.image}"
                         alt="${product.namaProduk}"
@@ -196,16 +46,16 @@ function displayProducts(filter) {
                 </a>
             </div>
         `;
-
     
-        if (index < 2) {
-            bigProductList.innerHTML += productCardBig; 
+        if (bigCardCount < 2) {
+            bigProductList.innerHTML += productCardBig;
+            bigCardCount++;
         } else {
-            smallProductList.innerHTML += productCardSmall; 
+            smallProductList.innerHTML += productCardSmall;
         }
     });
+    
 }
-
 
 function setActiveFilter(filter) {
     document.querySelectorAll(".filter").forEach(link => {
@@ -214,6 +64,14 @@ function setActiveFilter(filter) {
             link.classList.add("active-filter");
         }
     });
+}
+
+function showProductDetail(productName) {
+    const product = products.data.find(p => p.namaProduk === productName);
+    if (product) {
+        localStorage.setItem('selectedProduct', JSON.stringify(product));
+        window.location.href = '/pages/detail-produk.html';
+    }
 }
 
 document.querySelectorAll(".filter").forEach(link => {
